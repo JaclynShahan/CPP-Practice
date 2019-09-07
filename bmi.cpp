2fabfa90145f49a739bedc6045845c83ac2cbeec
Jaclyn <jaclyndshahan@gmail.com>
2019-09-06 21:59:42 -0500

    bmi calculator

---
+++ bmi.cpp
@@ -0,0 +1,26 @@
+int main() {
+  
+  double height, weight, bmi;
+  
+  // Ask user for their height
+  
+  std::cout << "Type in your height (m): ";
+  std::cin >> height;
+  
+  // Now ask the user for their weight and calculate BMI
+  
+  std::cout << "Type in your weight (kg):";
+  std::cin >> weight;
+  
+  bmi = weight / (height * height);
+  std::cout << "Your BMI is " << bmi << "\n";
+  
+  
+  
+  
+  
+  
+  
+  return 0;
+
+}
