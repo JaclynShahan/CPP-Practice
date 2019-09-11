6a547e6cac9abaefd4b2a0ec80fb2100178f4c5b
Jaclyn <jaclyndshahan@gmail.com>
2019-09-10 19:36:47 -0500

    square exercise

---
+++ square.cpp
@@ -0,0 +1,15 @@
+int main() {
+  
+  int i = 0;
+  int square = 0;
+  
+  // Write a while loop here:
+  while (i <= 9) {
+    square = i * i;
+   std::cout << "The square root of " << i << " is " << square << ".\n";
+    i++;
+  }
+  
+  
+  
+}
